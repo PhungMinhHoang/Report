d635b98097b2f838b61834bbc2c735b7ab14e109
hotroit <hotroit@vttek.vn>
2019-10-24 16:42:59 +0700

    Them chuc nang export excel

--- resources/js/views/DuAn.vue
+++ resources/js/views/DuAn.vue
@@ -1,42 +1,88 @@
 <template>
-    <div class="animated">
-      <b-card>
-        <b-card-header>
-          <i class="far fa-list-alt fa-2x"></i>
-          <h2>Danh sách đề tài/dự án</h2>
-          <div class="card-header-actions">
-            <b-button id="add-button" variant="success" @click="addModal = true">
-              <b-tooltip target="add-button" variant="dark">
-                <strong>Thêm đề tài/dự án mới</strong>
+  <div class="animated">
+    <b-card>
+      <b-card-header>
+        <i class="far fa-list-alt fa-2x"></i>
+        <h2>Danh sách đề tài/dự án</h2>
+        <div class="card-header-actions">
+          <!-- Export -->
+          <JsonExcel :data="json_data" :fields="json_fields" worksheet="Sheet" name="Danh sách dự án" style="display:inline">
+            <b-button id="export-button" variant="success" @click="1">
+              <b-tooltip target="export-button" variant="dark">
+                <strong>Xuất Excel</strong>
               </b-tooltip>
-              <i class="fas fa-plus fa-lg"></i>
+              <i class="fas fa-download fa-lg"></i>
             </b-button>
-            <!-- Modal add du an -->
-            <b-modal id="add-du-an-modal" title="Thêm dự án mới" size="md" v-model="addModal" hide-footer>
-              <AddDuAnForm />
-            </b-modal>
-          </div>
-        </b-card-header>
-        <b-card-body>
-          <DuAnTable />
-        </b-card-body>
-      </b-card>
-    </div>
+          </JsonExcel>
+          <!-- Add -->
+          <b-button id="add-button" variant="success" @click="addModal = true">
+            <b-tooltip target="add-button" variant="dark">
+              <strong>Thêm đề tài/dự án mới</strong>
+            </b-tooltip>
+            <i class="fas fa-plus fa-lg"></i>
+          </b-button>
+          <!-- Modal add du an -->
+          <b-modal
+            id="add-du-an-modal"
+            title="Thêm dự án mới"
+            size="md"
+            v-model="addModal"
+            hide-footer
+          >
+            <AddDuAnForm />
+          </b-modal>
+        </div>
+      </b-card-header>
+      <b-card-body>
+        <DuAnTable />
+      </b-card-body>
+    </b-card>
+  </div>
 </template>
 
 <script>
 import DuAnTable from "../components/table/DuAnTable";
 import AddDuAnForm from "../components/form/AddDuAnForm";
+import JsonExcel from "vue-json-excel";
 export default {
   name: "DuAn",
   components: {
     DuAnTable,
-    AddDuAnForm
+    AddDuAnForm,
+    JsonExcel
   },
-  data(){
+  data() {
     return {
       addModal: false,
-    }
+      json_fields: {
+        Id: "id",
+        "Mã đề tài": "ma_de_tai",
+        "Tên đề tài": "ten_de_tai",
+        "Trạng thái": {
+          field: "trang_thai",
+          callback: value => {
+            if (value == true) return "Đang thực hiện";
+            else return "Đã kết thúc";
+          }
+        },
+        "Tên đơn vị": "ten_don_vi",
+        "Phụ trách QA": "QA"
+      },
+      json_data: [],
+      json_meta: [
+        [
+          {
+            key: "charset",
+            value: "utf-8"
+          }
+        ]
+      ]
+    };
+  },
+  mounted() {
+    axios.get("/du-an").then(response => {
+      this.json_data = response.data;
+    });
   }
 };
 </script>
--- resources/js/views/KPIQuyTrinh.vue
+++ resources/js/views/KPIQuyTrinh.vue
@@ -5,6 +5,17 @@
           <b-card-header>
             <i class="far fa-list-alt fa-2x"></i>
             <h2>Kết quả KPI đánh giá Quy trình</h2>
+            <div class="card-header-actions">
+              <!-- Export -->
+              <JsonExcel :data="json_data" :fields="json_fields" worksheet="Sheet" name="Kết quả KPI đánh giá Quy trình" style="display:inline">
+                <b-button id="export-button" variant="success" @click="1">
+                  <b-tooltip target="export-button" variant="dark">
+                    <strong>Xuất Excel</strong>
+                  </b-tooltip>
+                  <i class="fas fa-download fa-lg"></i>
+                </b-button>
+              </JsonExcel>
+            </div>
           </b-card-header>
           <b-card-body>
             <KPIQuyTrinhTable />
@@ -16,14 +27,39 @@
 
 <script>
 import KPIQuyTrinhTable from "../components/table/KPIQuyTrinhTable";
+import JsonExcel from "vue-json-excel";
 export default {
   name: "DuAn",
   components: {
-    KPIQuyTrinhTable
+    KPIQuyTrinhTable,
+    JsonExcel
   },
   data() {
-    return {};
-  }
+    return {
+        json_fields: {
+        Id: "id",
+        'Tên dự án':'ten_du_an',
+        'Tên quy trình':'ten_quy_trinh',
+        'Tên module':'ten_module',
+        'Điểm':'diem',
+        'Thời gian':'thoigian'
+      },
+      json_data: [],
+      json_meta: [
+        [
+          {
+            key: "charset",
+            value: "utf-8"
+          }
+        ]
+      ]
+    };
+  },
+  mounted() {
+    axios.get("/kpi-quy-trinh").then(response => {
+      this.json_data = response.data;
+    });
+  },
 };
 </script>
 
